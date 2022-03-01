ad2d6a339ae30d4dc9873eb34ac71969e922f3af
Troy Moench <brewersarenumber1@gmail.com>
2022-02-28 21:39:12 -0600

    Added order module

---
+++ src/seawarfare/order.py
@@ -0,0 +1,51 @@
+from abc import ABC
+from datetime import datetime
+from attrs import define
+
+
+@define
+class Order(ABC):
+    id: str
+    extime: datetime
+
+    def get_id(self) -> str:
+        return self.id
+
+    def get_extime(self) -> datetime:
+        return self.extime
+
+    def print(self) -> None:
+        print(f"id: {self.id} extime: {self.extime}")
+
+
+@define
+class DeployShip(Order):
+    start_x: float
+    start_y: float
+    heading: float
+    speed: float
+
+
+@define
+class DeployAircraft(Order):
+    heading: float
+    speed: float
+    altitude: float
+
+
+@define
+class ChangeShip(Order):
+    heading: float
+    speed: float
+
+
+@define
+class ChangeAircraft(Order):
+    heading: float
+    speed: float
+    altitude: float
+
+
+@define
+class LandAircraft(Order):
+    ship_id: str
---
+++ tests/test_order.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from seawarfare.order import DeployShip
+
+
+def test_create_deploy_ship():
+    tm = datetime(2015, 10, 21, 17, 2, 0)
+    order = DeployShip(
+        id="CGN-39", extime=tm, start_x=0.0, start_y=0.0, heading=0.0, speed=50
+    )
+    order.print()
+    assert order.get_id() == "CGN-39"
+    assert order.get_extime() == datetime(2015, 10, 21, 17, 2, 0)
