43f731be0f59ca7f5e6699513a94bebb3dad689e
Troy Moench <brewersarenumber1@gmail.com>
2022-11-11 00:56:40 -0600

    Converted setup.py to declarative metadata using setup-py-upgrade and setup-cfg-fmt

--- setup.py
+++ setup.py
@@ -1,13 +1,2 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="seawarfare",
-    version="0.0.1",
-    description="Seawarfare Simulation in Python",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    entry_points="""
-    [console_scripts]
-    seawarfare=seawarfare.main:main
-    """,
-)
+from setuptools import setup
+setup()
